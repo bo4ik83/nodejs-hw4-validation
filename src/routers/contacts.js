53e543b9628360b2e1208561ccd4d7ee32e1319c
Denis Bochev <denisbo4ev@gmail.com>
2025-03-12 13:24:21 +0200

    add isValidId

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -7,13 +7,14 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsController));
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 router.post('/', ctrlWrapper(createContactController));
-router.patch('/:contactId', ctrlWrapper(patchContactController));
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.patch('/:contactId', isValidId, ctrlWrapper(patchContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
