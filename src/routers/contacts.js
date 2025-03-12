6844a55306ecf5cd8eefc5a79228430480fefd0a
Denis Bochev <denisbo4ev@gmail.com>
2025-03-12 15:06:23 +0200

    add validateBody

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,15 +6,26 @@ import {
   deleteContactController,
   patchContactController,
 } from '../controllers/contacts.js';
-import ctrlWrapper from '../utils/ctrlWrapper.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { contactSchema, updateContactSchema } from '../validation/contact.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const router = express.Router();
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-router.post('/', ctrlWrapper(createContactController));
-router.patch('/:contactId', isValidId, ctrlWrapper(patchContactController));
+router.post(
+  '/',
+  validateBody(contactSchema),
+  ctrlWrapper(createContactController),
+);
+router.patch(
+  '/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(patchContactController),
+);
 router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
