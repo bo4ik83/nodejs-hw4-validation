6db6d6b01d1c2fcff29db1b606a1b673cbb93220
Denis Bochev <denisbo4ev@gmail.com>
2025-03-14 20:44:49 +0200

    parsePaginationParams

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,9 +6,15 @@ import {
   patchContact,
   deleteContact,
 } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.params);
+
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+  });
   res.status(200).json({
     status: 200,
     message: 'Successfully found contacts!',
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,38 +1,45 @@
 import Contact from '../db/models/contact.js';
 
-export const getAllContacts = async () => {
-  return await Contact.find({});
+export const getAllContacts = async ({ page, perPage }) => {
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const [total, contacts] = await Promise.all([
+    Contact.countDocuments(),
+    Contact.find({}).skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(total / perPage);
+
+  return {
+    contacts,
+    total,
+    page,
+    perPage,
+    totalPages,
+    hasNextPage: totalPages > page,
+    hasPreviousPage: page > 1,
+  };
 };
 
 export const getContactById = async (contactId) => {
-  const contact = await Contact.findById(contactId);
-  return contact;
+  return await Contact.findById(contactId);
 };
 
 export const createContact = async (contactData) => {
-  const newContact = new Contact(contactData);
-  return await newContact.save();
+  return await Contact.create(contactData);
 };
 
 export const patchContact = async (contactId, updateData) => {
-  const patchedContact = await Contact.findByIdAndUpdate(
-    contactId,
-    updateData,
-    {
-      new: true,
-      runValidators: true,
-    },
-  );
-
-  return patchedContact;
+  return await Contact.findByIdAndUpdate(contactId, updateData, {
+    new: true,
+    runValidators: true,
+  });
 };
 
 export const deleteContact = async (contactId) => {
-  const deletedContact = await Contact.findByIdAndDelete(contactId);
-
-  return deletedContact;
+  return await Contact.findByIdAndDelete(contactId);
 };
 
 export const getContactByPhoneNumber = async (phoneNumber) => {
-  return Contact.findOne({ phoneNumber });
+  return await Contact.findOne({ phoneNumber });
 };
